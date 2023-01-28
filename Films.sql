c80549926211bb11af3be5b1355bd1b8c7f02f25
Jack <L00158710@atu.ie>
2023-01-28 16:27:31 +0000

    SQL

---
+++ Films.sql
@@ -0,0 +1,16 @@
+create table Films (
+	id INT,
+	name VARCHAR(50),
+	director VARCHAR(50),
+	duration VARCHAR(50),
+	trailer_url VARCHAR(1000)
+);
+insert into Films (id, name, director, duration, trailer_url) values (1, 'Inception', 'Christopher Nolan', '2:28', 'https://www.youtube.com/watch?v=YoHD9XEInc0');
+insert into Films (id, name, director, duration, trailer_url) values (2, 'Avatar: The Way of the Water', 'James Cameron', '3:13', 'https://www.youtube.com/watch?v=d9MyW72ELq0');
+insert into Films (id, name, director, duration, trailer_url) values (3, 'The Fabelmans', 'Steven Spielberg', '2:31', 'https://www.youtube.com/watch?v=D1G2iLSzOe8');
+insert into Films (id, name, director, duration, trailer_url) values (4, 'Puss in Boots: The Last Wish', 'Joel Crawford', '1:42', 'https://www.youtube.com/watch?v=RqrXhwS33yc');
+insert into Films (id, name, director, duration, trailer_url) values (5, 'Babylon', 'Damien Chazelle', '3:09', 'https://www.youtube.com/watch?v=5muQK7CuFtY');
+insert into Films (id, name, director, duration, trailer_url) values (6, 'Life is a Jungle', 'Robert D. Hanna', '1:41', 'https://www.youtube.com/watch?v=rwyUzHEG_XY');
+insert into Films (id, name, director, duration, trailer_url) values (7, 'Guardians of the Galaxy Vol. 3', 'James Gunn', '2:22', 'https://www.youtube.com/watch?v=u3V5KDHRQvk');
+insert into Films (id, name, director, duration, trailer_url) values (8, 'Ant-Man and The Wasp: Quantumania', 'Peyton Reed', '2:05', 'https://www.youtube.com/watch?v=ZlNFpri-Y40');
+insert into Films (id, name, director, duration, trailer_url) values (9, 'Spider-Man Across the Spiderverse', 'Joaqium Dos Santos', '7:54', 'https://www.youtube.com/watch?v=cqGjhVJWtEg');
