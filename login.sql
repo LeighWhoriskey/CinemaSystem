93385e37d00c04ccafd0bce2a8a66eaaccf727b0
Jackmacintyre3 <103225059+Jackmacintyre3@users.noreply.github.com>
2023-02-08 11:14:25 +0000

    Add files via upload
    customers and login sql file

---
+++ login.sql
@@ -0,0 +1,8 @@
+create table login (
+	id INT,
+	email VARCHAR(50),
+	password VARCHAR(50)
+);
+insert into login (id, email, password) values (1, 'guest@decade.com', 'guest');
+insert into login (id, email, password) values (2, 'admin@decade.com', 'admin');
+insert into login (id, email, password) values (3, 'manager@decade.com', 'manager');
