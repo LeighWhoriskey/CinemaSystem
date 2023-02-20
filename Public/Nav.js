449f80254558a59f9ec8893a269d0d0ac2a90050
Jack <L00158710@atu.ie>
2023-02-20 09:29:38 +0000

    home page beginning

--- Public/Nav.js
+++ Public/Nav.js
@@ -1,14 +1,16 @@
 $("document").ready(function(){
+    $("#NavBar").addClass("navbar");
 
-    "<img src = images/logo.png height = 60px;') " +
-    $("#NavBar").append("<a href='Home.html'>Home</a> | " +
-        "<a href='Screening.html'>Screenings</a> | " +
-        "<a href='Booking.html'>Bookings</a> | " +
-        "<a href='Movie.html'>Movie</a> | " +
-        "<a href='Login.html'>Login</a> | " +
-        "<a href='CRUD.html'>CRUD</a> " +
-        "<hr>");
+    $("#NavBar").append("<img src='images/logo.png' height='60px'>");
+    $("#NavBar").append("<div class='navbar-items'>" +
+        "<a href='Home.html'>Home</a>" +
+        "<a href='Screening.html'>Screenings</a>" +
+        "<a href='Booking.html'>Bookings</a>" +
+        "<a href='Login.html'>Login</a>" +
+        "<a href='CRUD.html'>CRUD</a>" +
+        "</div>");
+
+        //$("#LogIn").append("a href<'Login.html'>Login</a>");
 
     $("#footer").append("<hr><p style='text-align: center'> Copyright © 2023 L00156477 - L00152828 - L00158710 - L00157862</p>");
-
 });
