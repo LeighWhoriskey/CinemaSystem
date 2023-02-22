6518b66dce615293319cd281b67d814d634bf3bb
Killian Dolan <L00157862@student.lyit.ie>
2023-02-22 20:15:14 +0000

    now showing page and movie details done and fixed getMovieDetails in  model

--- Public/Movie.js
+++ Public/Movie.js
@@ -1,19 +1,49 @@
 $("document").ready(function(){
-
+var colour;
     
     $.getJSON("http://localhost:3000/movies",function(data){
-        var output ="";
+    
        
         $.each(data, function(i, value){
-            $("#movies").append(`<div class="images"style=" display:inline-block; padding:0px; margin: 0;  left: 100px;  position: relative;"><img src='images/${value.id}.jpg'style='width: 230px; margin:0px; display:'></img></div>`);
-            //$("#movies").append(`<div class="name"style="  display:inline-block; padding:0px; margin: 0; left: 150px; bottom:220px;  position: relative;"><h3 style="font-size:25px;">${value.name}</h3></div>`);
-            $("#movies").append(`<div class="details"style=" display:inline-block; padding:0px; margin: 0; left: 150px; bottom:20px;    position: relative;">
-            <svg height="250" width="250">
-            <circle cx="50" cy="55" r="25" stroke="black" fill="red" />
-            <text x="30" y="20" fill="black"font-size = 25px>${value.name}</text>
-            <text x="35" y="60" fill="black">${value.age}</text>
-            <text x="100" y="60" fill="black" font-size = 15px>${value.duration} hours</div></text>
+           
+            $("#movies").append(`<div class="images"style=" display:inline-block; padding:0px; margin-top: 20px;  left: 100px;  position: relative;"><a href="http://localhost:3000/Movie-Detail.html?id=${value.id}"><img src='images/${value.id}.jpg'style='width: 230px; height: 300px; margin:0px; display:'></img></a></div>`);
+            $("#movies").append(`<div class="name"style=" font-size:25px;left:150px; top:-250px; position: relative; display:inline-block; ">${value.name}</div>`);
+            if(value.age == "12A" )
+            {
+            $("#movies").append(`<div class="details"style="  left:350px; top:-270px; display: block; padding:0px; margin: 0; position: relative;">
+            <svg >
+            <circle cx="50" cy="55" r="25" stroke="black" fill="#ff4500" />
+            <text text-anchor="middle" x="50" y="60" fill="white">${value.age}</text>
+            <text x="100" y="60" fill="black" font-size = 20px>${value.duration} hours</div></text>
           </svg></div>`);
+            }
+            if(value.age == "18" )
+            {
+            $("#movies").append(`<div class="details"style="  left:350px; top:-270px; display: block; padding:0px; margin: 0; position: relative;">
+            <svg >
+            <circle cx="50" cy="55" r="25" stroke="black" fill="red" />
+            <text text-anchor="middle" x="50" y="60" fill="white">${value.age}</text>
+            <text x="100" y="60" fill="black" font-size = 20px>${value.duration} hours</div></text>
+          </svg></div>`);
+            }
+            if(value.age == "PG" )
+            {
+            $("#movies").append(`<div class="details"style="  left:350px; top:-270px; display: block; padding:0px; margin: 0; position: relative;">
+            <svg >
+            <circle cx="50" cy="55" r="25" stroke="black" fill="#008000" />
+            <text text-anchor="middle" x="50" y="60" fill="white">${value.age}</text>
+            <text x="100" y="60" fill="black" font-size = 20px>${value.duration} hours</div></text>
+          </svg></div>`);
+            }
+            if(value.age == "U" )
+            {
+            $("#movies").append(`<div class="details"style="  left:350px; top:-270px; display: block; padding:0px; margin: 0; position: relative;">
+            <svg >
+            <circle cx="50" cy="55" r="25" stroke="black" fill="#0000ff" />
+            <text text-anchor="middle" x="50" y="60" fill="white">${value.age}</text>
+            <text x="100" y="60" fill="black" font-size = 20px>${value.duration} hours</div></text>
+          </svg></div>`);
+            }
           $("#movies").append("<hr>");
         
           
@@ -23,10 +53,8 @@ $("document").ready(function(){
          
         });
        
-        /*$(".trailer").click(function(event){
-         
-            window.location.replace(event.target.value);
-        });*/
+       
+     
     });
 
     
