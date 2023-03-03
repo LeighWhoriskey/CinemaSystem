d998f71e4113ea647c31879c54e6f493d4b070c3
Killian Dolan <L00157862@student.lyit.ie>
2023-03-03 16:24:06 +0000

    added times and screen to now showing

--- Public/Movie.js
+++ Public/Movie.js
@@ -11,15 +11,16 @@ $("document").ready(function(){
         // Create new div for movie details
         var screenTime = "Screen "+value.Screen_id+ " " + value.Time;
         $("#movies").append("<hr style='border: 2px solid black'>");
-        $("#movies").append(`<div id="${value.id}" class="movie" style="display:inline-block;">`);
-        $("#" + value.id).append(`<div class="images" style="display:inline-block; padding:0px; margin-top: 20px;"><a href="http://localhost:3000/Movie-Detail.html?id=${value.id}"><img src='images/${value.id}.jpg' style='width: 230px; height: 300px; margin:0px; display:'></a></div>`);
-        $("#" + value.id).append(`<div class="details" style="width: 200px; height:150px;display:inline-block; margin-left:20px;"><h2>${value.name}</h2>
-        
-        <svg height="200px" width="200px"> <circle cx="25" cy="27" r="25" stroke="black" fill="${getAgeColor(value.age)}"></circle> <text text-anchor="middle" x="25" y="30" fill="white">${value.age}</text> <text x="100" y="30" fill="black" font-size="20px">${value.duration} hours</text></svg></div>`);
-        $("#" + value.id).append(`<div class="time" style=" padding:0px; margin-left: 600px; "><div>${screenTime}</div></div>`);
+$("#movies").append(`<div class="movie-container"><div class="images" ><a href="http://localhost:3000/Movie-Detail.html?id=${value.id}"><img src='images/${value.id}.jpg'style="width:200px; height:300px;"></a></div>
+                      <div class="details" ><h2>${value.name}</h2>
+                      <svg height="100px" width="200px"> <circle cx="30" cy="27" r="25" stroke="black" fill="${getAgeColor(value.age)}"></circle> <text text-anchor="middle" x="30" y="30" fill="white">${value.age}</text> <text x="100" y="30" fill="black" font-size="20px">${value.duration} hours</text></svg></div>
+                      <div class="screens"id="${value.id}" ><div class="time" id="time-${value.id}" style=""><div>${screenTime}</div></div></div>
+                          </div>`);
       }
-    });
+      });
+
   });
+  
 });
 
 function getAgeColor(age) {
