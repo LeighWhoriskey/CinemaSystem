3e614ed0b7768af2a1e047d8ca4c038ad65fa36d
Killian Dolan <L00157862@student.lyit.ie>
2023-03-23 14:17:45 +0000

    test

--- Public/Movie.js
+++ Public/Movie.js
@@ -38,16 +38,13 @@ $("document").ready(function(){
         var screenTime = "Screen "+value.Screen_id+ " " + value.Time;
 
         $("#movies").append("<hr style='border: 2px solid black'>");
-$("#movies").append(`<div class="movie-container"><div class="images" ><a href="http://localhost:3000/Movie-Detail.html?id=${value.id}"><img src='images/${value.id}.jpg'style="width:200px; height:300px;"></a></div>
+$("#movies").append(`<div class="movie-container" id=${value.id}><div class="images" ><a href="http://localhost:3000/Movie-Detail.html?id=${value.id}"><img src='images/${value.id}.jpg'style="width:200px; height:300px;"></a></div>
                       <div class="details" ><h2>${value.name}</h2>
                       <svg height="100px" width="200px"> <circle cx="30" cy="27" r="25" stroke="black" fill="${getAgeColor(value.age)}"></circle> <text text-anchor="middle" x="30" y="30" fill="white">${value.age}</text> <text x="100" y="30" fill="black" font-size="20px">${value.duration} hours</text></svg></div>
                       <div class="screens"id="${value.id}" ><div class="time" id="time-${value.id}" style=""><div>${screenTime}</div></div></div>
                           </div>`);
       }
-      }).fail(function(jqXHR, textStatus, errorThrown) {
-        console.error("Error while fetching movies:", errorThrown);
-        alert("Error while fetching movies:", errorThrown);
-  })
+    });
   });
 });
 });
