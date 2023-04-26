a49f3fed56ec6da2bf3cb3477594903089d57bd7
HeIsMilkMan <bobysimpson4@gmail.com>
2023-04-26 16:50:32 +0100

    I did something cant remember

--- Public/BookingPayment.js
+++ Public/BookingPayment.js
@@ -50,64 +50,70 @@ $("document").ready(function(){
     
     
     $("#paymentButton").on("click",function(){
-        if(custId != 0){
 
-            $.ajax({
-                url: "/checkForOverBooking",
-                cache: false,
-                dataType: "json",
-                type: "POST",
-                data:{
-                  cust_id: custId,
-                  seats: totalTickets,
-                  screeningId: screeningId
-                },
-                success: function(res){
-                    if(Object.keys(res).length === 0){
-                        console.log(res);
-                        $.ajax({
-                            url: "/makeBooking",
-                            cache: false,
-                            dataType: "json",
-                            type: "POST",
-                            data:{
-                            ScreenId: screenNum,
-                            Seats: totalTickets,
-                            CustID: custId,
-                            totalSeats: totalSeats,
-                            screeningId: screeningId
-                            },
-                            success: function(res){
-                            console.log(res);
-                            alert("Booking has been Made");
+        if($("#cardName").val()== 0 || $("#cardNumber").val()== 0 || $("#month").val()== 0 || $("#year").val()== 0 || $("#cvv").val()== 0){
+            $("#error").empty().append("<p style='color:red'>Please enter all details</p>");
+        }else{
 
-
-                            //not right need to fix some stuff
-                            //will look and see if this is a viable option
-                            // Email.send({
-                            //     Host : "smtp.gmail.com",
-                            //     Username : "decadecinema@gmail.com",
-                            //     Password : "Decade247",
-                            //     To : 'bobysimpson4@gmail.com',
-                            //     From : "test?",
-                            //     Subject : "Test email",
-                            //     Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
-                            //     }).then(
-                            //         message => alert(message)
-                            //     );
-                            
-                            }
-                        });
-                    }else{
-                        $("#error").empty();
-                        $("#error").append("<p style='color:red'>Booking already Exists");
-                    }
-                  
-                }
-            });
+            var cvvReg = /^[0-9]{3}/
+            var cardReg = /^[0-9]{16}/
+            if(cvvReg.test($("#cvv").val()) &&  cardReg.test($("#cardNumber").val())){
+                console.log("test")
             
-
-        }
+                $.ajax({
+                    url: "/checkForOverBooking",
+                    cache: false,
+                    dataType: "json",
+                    type: "POST",
+                    data:{
+                        cust_id: custId,
+                        seats: totalTickets,
+                        screeningId: screeningId
+                    },
+                    success: function(res){
+                        if(Object.keys(res).length === 0){
+                            console.log(res);
+                            $.ajax({
+                                url: "/makeBooking",
+                                cache: false,
+                                dataType: "json",
+                                type: "POST",
+                                data:{
+                                ScreenId: screenNum,
+                                Seats: totalTickets,
+                                CustID: custId,
+                                totalSeats: totalSeats,
+                                screeningId: screeningId
+                                },
+                                success: function(res){
+                                console.log(res);
+                                alert("Booking has been Made");
         
+        
+                                //not right need to fix some stuff
+                                //will look and see if this is a viable option
+                                // Email.send({
+                                //     Host : "smtp.gmail.com",
+                                //     Username : "decadecinema@gmail.com",
+                                //     Password : "Decade247",
+                                //     To : 'bobysimpson4@gmail.com',
+                                //     From : "test?",
+                                //     Subject : "Test email",
+                                //     Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+                                //     }).then(
+                                //         message => alert(message)
+                                //     );
+                                
+                                }
+                            });
+                        }else{
+                            $("#error").empty().append("<p style='color:red'>Booking already Exists</p>");
+                        }  
+                    }
+                }); 
+            }else{
+                $("#error").empty().append("<p style='color:red'>CardNumber or CVV Incorrect</p>");
+            }
+        }
     });
 });
--- Public/Email.js
+++ Public/Email.js
@@ -17,16 +17,19 @@ $("document").ready(function(){
 
  
 
-    console.log("test")
 
     $("#submit").on("click",function(){
         if($("#Name").val() == 0 || $("#Phone").val() == 0 || $("#EmailAddress").val() == 0){
-            $("#error").empty();
-            $("#error").append("<p style='color:red'>One or more Information is Missing</p>");
+            $("#error").empty().append("<p style='color:red'>One or more Information is Missing</p>");
         }else{
-            window.location.href ="BookingPayment.html?&adult="+aTicket+"&child="+ cTicket +"&ScreenNum=" 
+            var emailReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i; 
+            if(emailReg.test($("#EmailAddress").val())){
+                window.location.href ="BookingPayment.html?&adult="+aTicket+"&child="+ cTicket +"&ScreenNum=" 
                 + screenNum +"&Screening=" + screeningId + "&film=" +filmId +"&custId="+ custId +"&date=" + dateIn +"&time=" + time 
                 +"&adultPrice="+aPrice+"&childPrice="+cPrice +"&totalseats=" + totalSeats + "&Email=" +$("#EmailAddress").val();
+            }else{
+                $("#error").empty().append("<p style='color:red'>Invalid Email</p>");
+            }
         }
     });
     
